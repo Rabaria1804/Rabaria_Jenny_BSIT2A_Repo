5355e30c30833551a0b6702bb268db710dad4920
Rabaria1804 <jennyrabaria@filamer.edu.ph>
2025-03-20 02:21:31 -0700

    Getting Functional

---
+++ Getting Functional/function.js
@@ -0,0 +1,47 @@
+// var dogName = "winnie"; 
+// var dogWeight = 25; 
+
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF ");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "mallows"; 
+// var dogWeight = 35; 
+
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF ");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "hans"; 
+// var dogWeight = 14; 
+
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF ");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+// var dogName = "max"; 
+// var dogWeight = 27; 
+
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF ");
+// } else {
+//     console.log(dogName + " says woof woof");
+// }
+
+function bark (name, weight) {
+    if (weight > 20) {
+        console.log(name + "says WOOF WOOF");
+    
+    } else {
+        console.log(name + "says woof woof");
+    }
+}
+
+bark("mallows", 35);
+bark("max", 27);
