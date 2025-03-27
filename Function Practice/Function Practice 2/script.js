c1075945fca565b0fed2261a3d0817f010c4b7e3
Rabaria_Jenny_BSIT2A_Repo <your-email@example.com>
2025-03-27 21:52:26 +0800

    Function Practice 2

---
+++ Function Practice/Function Practice 2/script.js
@@ -0,0 +1,8 @@
+function printName(firstname, lastname) {
+    var fullname = firstname + lastname;
+    return fullname
+}
+
+var first = 'Jenny';
+var lastname = 'Rabaria';
+console.log('My Fullname is' + printName (first, lastname))
